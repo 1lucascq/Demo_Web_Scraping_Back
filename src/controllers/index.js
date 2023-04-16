4d40bf94698d0b7cbcace56bc87c38f82bcdad32
Lucas Quadros <1lucascq@gmail.com>
2023-04-16 18:31:49 -0300

    feat: create controller function to manage requests and response

---
+++ src/controllers/index.js
@@ -0,0 +1,20 @@
+const Services = require('../services');
+
+const getData = async (req, res, _next) => {
+    try {
+        const product = req.path;
+        const productsData = await Services.getData(product);
+
+        if (!productsData) {
+            return res.status(404).json({ message: `Não foram encontrados dados relacionados a ${product.slice(1)}.`});
+        }
+
+        return res.status(200).json(productsData);
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message: "Erro no servidor."});
+    }
+}
+
+module.exports = { getData };
