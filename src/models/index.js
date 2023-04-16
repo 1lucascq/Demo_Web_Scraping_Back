ce9e7b1848d8ba6ee64517549839ee658c2029c9
Lucas Quadros <1lucascq@gmail.com>
2023-04-16 18:33:05 -0300

    feat: use puppeteer to get buscape data

---
+++ src/models/index.js
@@ -0,0 +1,29 @@
+const puppeteer = require("puppeteer");
+
+const getData = async (product) => {
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`https://www.buscape.com.br${product}`);
+
+        const productData = await page.$$eval('[data-testid="product-card"]', (productCard) => {
+            return productCard.map((card) => {
+                const name = card.querySelector('[data-testid="product-card::name"]').textContent;
+                const price = card.querySelector('[data-testid="product-card::price"]').textContent;
+                const image = card.querySelector('[data-testid="product-card::image"] img').src;
+
+                return {name, price, image}
+            })
+        });
+
+        await browser.close();
+
+        return productData;
+    } catch (err) {
+        console.log('error log')
+        console.error(err);
+        return `Failed to retrieve data in the ${product} endpoint}`
+    }
+};
+
+module.exports = { getData };
