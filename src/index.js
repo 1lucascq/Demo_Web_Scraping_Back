ab634b39cd4e2f18d4a8544e5a0b0c9f7505ecab
lucas.quadros <1lucascq@gmail.com.br>
2024-09-10 11:33:46 -0300

    refactor: Update .gitignore and src/index.js for the deployment work properly

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const routes = require('./routes');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(cors());
-
 app.use('/', routes);
 
 app.listen(PORT, () => console.log(`On: ${PORT}`));
+
+module.exports = app;
